9a283d3333764ba9632bb0204d5953172c01413a
tesfatsion <tesfatsiongudeta7@gmail.com>
2025-03-05 07:02:21 -0800

    add Tailwind CSS configuration and custom font int egration

--- todo-frontend/src/pages/Home.tsx
+++ todo-frontend/src/pages/Home.tsx
@@ -5,11 +5,13 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="h-screen flex justify-center flex-col items-center gap-10">
-      <div className="text-2xl">
+      <div className="font-poppins text-4xl font-bold">
         GET YOU TASKS ORGANISED WITH A SIMPLE TODOS WEB APP!
       </div>
       <Link to="/todos">
-        <Button>login</Button>
+        <Button size="lg" className="text-2xl cursor-pointer">
+          login
+        </Button>
       </Link>
     </div>
   );
